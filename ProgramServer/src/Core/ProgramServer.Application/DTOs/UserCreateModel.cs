ef9cc7aa7f8a81b13c78f32549cccb4b15542fac
Ani <annietkhelidze@outlook.com>
2024-02-17 14:20:28 +0400

    test

--- ProgramServer/src/Core/ProgramServer.Application/DTOs/UserCreateModel.cs
+++ ProgramServer/src/Core/ProgramServer.Application/DTOs/UserCreateModel.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using FluentValidation;
-using ProgramServer.Domain.Roles;
-using ProgramServer.Domain.SubjectUsers;
+﻿using FluentValidation;
 
 namespace ProgramServer.Application.DTOs
 {
