91bfcb59633ff6e5653b808e2b687e8f5c9ac377
Ani <annietkhelidze@outlook.com>
2024-07-13 04:13:35 +0400

    small fixup

--- ProgramServer/src/Core/ProgramServer.Application/DTOs/SurveyModel.cs
+++ ProgramServer/src/Core/ProgramServer.Application/DTOs/SurveyModel.cs
@@ -5,6 +5,7 @@ namespace ProgramServer.Application.DTOs
 {
     public class SurveyModel
     {
+        public int Id { get; set; }
         public string Subject { get; set; }
         public string Question { get; set; }
     }
